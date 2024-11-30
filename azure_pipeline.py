2334efb2d49c5c47c1113425adc7377217fb07a8
Jeong Shin <jyshin@stanford.edu>
2024-11-29 23:19:23 -0800

    processing most directories with azure, complications with faded_ripped dir

--- azure_pipeline.py
+++ azure_pipeline.py
@@ -1,8 +1,8 @@
 import os
+import time
 from PIL import Image
 
 def convert_to_supported_format(input_path):
-    supported_formats = ["JPEG", "JPG", "PNG", "BMP", "TIFF", "HEIF"]
     converted_path = input_path.replace(".jp2", ".jpg")
 
     image = Image.open(input_path)
@@ -36,9 +36,44 @@ def main():
         endpoint=endpoint, credential=AzureKeyCredential(key)
     )
 
-    # TODO FILL IN
-    input_dir = ""
-    output_dir = ""
+    # # FOR INDIVIDUAL FILES
+    # input_path = "evaluation_data/complex_layout/jaic__granada_pioneer_03nov1944.jp2"
+    # if not os.path.isfile(input_path):
+    #     print("The specified file does not exist.")
+    #     return
+
+    # output_dir = "output"
+    # os.makedirs(output_dir, exist_ok=True)
+
+    # if input_path.lower().endswith(".jp2"):
+    #     file_size_mb = os.path.getsize(input_path) / (1024 * 1024)
+    #     print("File size: ", file_size_mb)
+
+    #     start_time = time.time()
+
+    #     converted_path = convert_to_supported_format(input_path)
+
+    #     output_file_name = os.path.splitext(os.path.basename(input_path))[0] + ".txt"
+    #     output_file_path = os.path.join(output_dir, output_file_name)
+
+    #     try:
+    #         extract_text(converted_path, output_file_path, document_analysis_client)
+    #         print(f"Text extraction complete. Saved to {output_file_path}")
+    #     except Exception as e:
+    #         print(f"Failed to process {input_path}: {e}")
+
+    #     end_time = time.time()
+    #     elapsed_time = end_time - start_time
+    #     print(f"Processing time for {os.path.basename(input_path)}: {elapsed_time:.2f} seconds")
+    #     print()
+
+    #     os.remove(converted_path)
+    # else:
+    #     print("The specified file is not a .jp2 file.")
+
+    # FOR DIRECTORIES
+    input_dir = "evaluation_data/simple_layout"
+    output_dir = "output/simple_layout"
     os.makedirs(output_dir, exist_ok=True)
 
     for filename in os.listdir(input_dir):
@@ -46,6 +81,8 @@ def main():
 
         if filename.lower().endswith((".jp2", ".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".heif")):
             print(f"Processing: {filename}")
+
+            start_time = time.time()
 
             if filename.lower().endswith(".jp2"):
                 file_path = convert_to_supported_format(file_path)
@@ -58,6 +95,11 @@ def main():
                 print(f"Text extraction complete for {filename}. Saved to {output_file_path}")
             except Exception as e:
                 print(f"Failed to process {filename}: {e}")
+
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"Processing time for {filename}: {elapsed_time:.2f} seconds")
+            print()
 
             if filename.lower().endswith(".jp2"):
                 os.remove(file_path)
